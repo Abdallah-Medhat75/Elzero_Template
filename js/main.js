5ef554a363dc45f279035723384b44cc864582e0
unknown <abdallah.medhata3@gmail.com>
2025-05-15 06:44:33 +0300

    Added Customized Scroll To Top Using JavaScript + Fixed Header Position

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
+// Start Selecting Elements
+let scrollToTopAnchor = document.querySelector(".scroll-to-top");
 let clickAnchor = document.querySelector("header .container ul li:nth-child(4)");
 let megaMenu = document.querySelector(".mega-menu")
-
-// Anchor Does Not Work Good With click event
-
+let landingSection = document.querySelector(".landing");
+// End Selecting Elements
 clickAnchor.onclick = function () {
     megaMenu.classList.toggle("active");
 }
+landingSection.onclick = function () {
+    megaMenu.classList.remove("active");
+}
+let statsP = document.querySelectorAll(".stats .container .box p:nth-child(2)");
+let enable = true;
+window.onscroll = function () {
+    if (enable) {
+        if (window.scrollY >= document.querySelector(".stats").offsetTop) {
+            for (let i = 0; i < statsP.length; i++) {
+                function countup(el) {
+                    el.textContent++;
+                    if (el.textContent === el.getAttribute("data-target")) {
+                        clearInterval(counter);
+                    }
+                }
+                let counter = setInterval(countup, 2100 / statsP[i].getAttribute("data-target"), statsP[i]);
+            }
+            enable = false;
+        }
+    }
+    if (window.scrollY >= 600) {
+        scrollToTopAnchor.style.cssText = "display: flex; visibility: visible; opacity: 1;";
+    } else {
+        scrollToTopAnchor.style.cssText = "display: flex; visibility: visible; opacity: 0; cursor: initial;"
+    }
+};
